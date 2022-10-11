42cbb228d731fb6200307222e433fa97c92f3fb9
Harshini <72506800+HarshiniGowda@users.noreply.github.com>
2022-10-12 02:28:42 +1000

    update-2

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+  apikey: "20957daa73739ee04a5f325baf22699d",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerHTML = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerHTML = description;
+    document.querySelector(".temp").innerHTML = temp + "°C";
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+  },
+};
