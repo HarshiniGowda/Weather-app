d8da68d69d2d56ac004273c4a5234f7c6059b6c9
Harshini <72506800+HarshiniGowda@users.noreply.github.com>
2022-10-13 11:28:47 +1000

    update-3

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerHTML = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -24,5 +23,22 @@ let weather = {
     document.querySelector(".humidity").innerHTML =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading ");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("mysore");
